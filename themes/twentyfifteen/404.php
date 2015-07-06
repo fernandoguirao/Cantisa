4051cc61160335e8de17e495e4dfb88a7410ec88
Fernando Guirao <fernando@bueninvento.es>
2015-07-06 12:17:44 +0200

    Cambio git funcionando

--- themes/twentyfifteen/404.php
+++ themes/twentyfifteen/404.php
@@ -15,7 +15,7 @@ get_header(); ?>
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyfifteen' ); ?></h1>
-				</header><!-- .page-header 2 -->
+				</header><!-- .page-header -->
 
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>

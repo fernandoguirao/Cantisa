1db53fe37d8b87e7a7a334c9f803e629ae089eaa
Fernando Guirao <fernando@bueninvento.es>
2015-07-13 06:31:23 +0200

    Cantisa2

---
+++ themes/cantisa2/producto.php
@@ -0,0 +1,51 @@
+<?php /* ?><?php Template Name: Producto ?><?php */ ?><?php get_header(); ?>
+<div id="producto">
+  <div class="block container-fluid posts-block lightgray-bgcolor">
+    <div class="row">
+      <div id="product-left" class="col-sm-4">
+        <p class="uppercase">< Nuestros productos</p>
+        <div class="spacing sp-sm"></div><img src="<?php echo get_template_directory_uri(); ?>/assets/misc/demo02/plane2.jpg" alt="" class="img-full"/>
+        <div class="block main-bgcolor light-color">
+          <div class="align-right">
+            <div class="btn btn-bordered">
+              <p class="text-xl lowercase">2mm<span class="uppercase text-sm">espesor</span></p>
+            </div>
+            <div class="spacing sp-sm"></div>
+            <p>Juntado en zigzag</p>
+            <p>Rollos de bla bla</p>
+            <p>Corte en sierra</p>
+            <div class="spacing sp-sm"></div>
+            <p class="uppercase">Descargar ficha <span class="fa fa-download"></span></p>
+          </div>
+          <div class="clearfix"></div>
+        </div><a href="" class="block gray-bgcolor block-stretch text-center">
+          <p class="uppercase">< Volver a productos</p></a>
+      </div>
+      <div id="product-right" class="col-sm-8">
+        <div class="spacing sp-md"></div>
+        <h2 class="uppercase">Rollos de <br/><span class="bolder">chapa de madera</span></h2>
+        <div class="spacing sp-md"></div>
+        <p>Nuestra filosofía es ser tu aspirina virtual para quitar tus dolores de cabeza y solucionar tus problemas.</p>
+        <p>Hemos formado a nuestros empleados para atender a tus peticiones y se te plantearan soluciones apropiadas.</p>
+        <div class="spacing sp-sm"></div>
+        <div class="btn btn-bordered btn-dark btn-nolink">Presentación</div>
+        <div class="spacing sp-sm"></div>
+        <p>Nuestra filosofía es ser tu aspirina virtual para quitar tus dolores de cabeza y solucionar tus problemas.</p>
+        <p>Hemos formado a nuestros empleados para atender a tus peticiones y se te plantearan soluciones apropiadas.</p>
+        <div class="spacing sp-sm"></div>
+        <div class="btn btn-bordered btn-dark btn-nolink">Acabados</div>
+        <div class="spacing sp-sm"></div>
+        <p>Nuestra filosofía es ser tu aspirina virtual para quitar tus dolores de cabeza y solucionar tus problemas.</p>
+        <p>Hemos formado a nuestros empleados para atender a tus peticiones y se te plantearan soluciones apropiadas.</p>
+        <div class="spacing sp-sm"></div>
+        <div id="galeria" class="row">
+          <div class="col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/misc/demo02/plane2.jpg" alt="" class="img-full"/></div>
+          <div class="col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/misc/demo02/plane2.jpg" alt="" class="img-full"/></div>
+          <div class="col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/misc/demo02/plane2.jpg" alt="" class="img-full"/></div>
+          <div class="col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/misc/demo02/plane2.jpg" alt="" class="img-full"/></div>
+          <div class="col-sm-4"><img src="<?php echo get_template_directory_uri(); ?>/assets/misc/demo02/plane2.jpg" alt="" class="img-full"/></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div><?php get_footer(); ?>
